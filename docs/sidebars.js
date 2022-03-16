3d39be7c4b17eaf8b175ea295bd541a61392fb7f
Kurtis Lew <kurtis.a.lew@gmail.com>
2022-03-15 22:08:42 -0700

    feat(behaviors): Add Tap-Dance behavior

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -27,6 +27,7 @@ module.exports = {
       "behaviors/mod-morph",
       "behaviors/sticky-key",
       "behaviors/sticky-layer",
+      "behaviors/tap-dance",
       "behaviors/caps-word",
       "behaviors/mouse-emulation",
       "behaviors/reset",
